0ffdbd59425b3ab932b94aed9e05544130a7f282
Julio A.P. <jalvarezpimiento@gmail.com>
2024-01-10 10:58:42 -0500

    First Commit and created Api

---
+++ ProyectoAngular/Controllers/TareaController.cs
@@ -0,0 +1,56 @@
+﻿using Azure.Core;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using Microsoft.EntityFrameworkCore;
+using ProyectoAngular.Models;
+
+namespace ProyectoAngular.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TareaController : ControllerBase
+    {
+        private readonly PruebasContext _pruebasContext;
+
+        public TareaController(PruebasContext pruebasContext)
+        {
+            _pruebasContext = pruebasContext;
+        }
+
+        [HttpGet]
+        [Route("Lista")]
+        public async Task<IActionResult> Lista()
+        {
+            var listarTareas = await _pruebasContext.Tareas.ToListAsync();
+            return Ok(listarTareas);
+        }
+
+        [HttpPost]
+        [Route("Agregar")]
+        public async Task<IActionResult> Agregar([FromBody] Tarea request)
+        {
+            await _pruebasContext.Tareas.AddAsync(request);
+            await _pruebasContext.SaveChangesAsync();
+            return Ok(request);
+        }
+
+        [HttpDelete]
+        [Route("Eliminar/{id:int}")]
+        public async Task<IActionResult> Eliminar(int id)
+        {
+            var tareaEliminar = await _pruebasContext.Tareas.FindAsync(id);
+
+            if (tareaEliminar == null)
+            {
+                return BadRequest("No existe la tarea");
+            }
+            else 
+            {
+                _pruebasContext.Tareas.Remove(tareaEliminar);
+                await _pruebasContext.SaveChangesAsync();
+                return Ok();
+            }
+        }
+    }
+}
---
+++ ProyectoAngular/Models/PruebasContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProyectoAngular.Models;
+
+public partial class PruebasContext : DbContext
+{
+    public PruebasContext()
+    {
+    }
+
+    public PruebasContext(DbContextOptions<PruebasContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Tarea> Tareas { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Tarea>(entity =>
+        {
+            entity.HasKey(e => e.IdTarea);
+
+            entity.ToTable("Tarea");
+
+            entity.Property(e => e.IdTarea).HasColumnName("id_tarea");
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("nombre");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
